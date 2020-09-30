606bd66aac6240d57d6e4eb1757c7b5443761617
anand.kumar1 <anand.kumar1@mheducation.com>
2020-09-30 09:27:07 +0530

    commit questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'take-exam';
+  numberOfQuestions = 40;
+  arrayQuestion = [];
+
+  submitted = false;
+
+  onSubmit() {
+    this.arrayQuestion = [];
+    this.submitted = true;
+    for (let i = 1; i <= this.numberOfQuestions; i++) {
+      this.arrayQuestion.push({
+        questionNo: i,
+        answered: false,
+        mark: 0,
+        status: 'unchecked'
+      });
+    }
+    console.log(this.arrayQuestion);
+  }
+
+  onCorrect(question){
+    const questionIndex = this.arrayQuestion.findIndex(obj => obj.questionNo === question.questionNo);
+    this.arrayQuestion[questionIndex].mark = 1;
+    this.arrayQuestion[questionIndex].status = 'correct';
+  }
+  onWrong(question){
+    const questionIndex = this.arrayQuestion.findIndex(obj => obj.questionNo === question.questionNo);
+    this.arrayQuestion[questionIndex].mark = 0;
+    this.arrayQuestion[questionIndex].status = 'wrong';
+  }
+
 }
