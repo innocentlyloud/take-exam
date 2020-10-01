5e5b9d6a3f7893b1f1d2cb74749c2c9f4d01d5d4
anand.kumar1 <anand.kumar1@mheducation.com>
2020-10-01 14:22:28 +0530

    add storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
@@ -7,7 +7,7 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'take-exam';
   numberOfQuestions = 40;
   arrayQuestion = [];
@@ -18,9 +18,21 @@ export class AppComponent {
   correctAnswer = [];
   wrongAnswer = [];
   unanswered = [];
-
+  myStorage = window.localStorage;
   constructor(private modalService: BsModalService) {}
+
+  ngOnInit(): void {
+    if (this.myStorage.getItem('questionans')) {
+      this.arrayQuestion = JSON.parse(this.myStorage.getItem('questionans'));
+      this.submitted = true;
+    }
+    if (this.myStorage.getItem('topic')) {
+      this.topic = this.myStorage.getItem('topic');
+    }
+  }
+
   onSubmit() {
+    this.myStorage.setItem('topic', this.topic);
     this.arrayQuestion = [];
     this.submitted = true;
     for (let i = 1; i <= this.numberOfQuestions; i++) {
@@ -28,7 +40,8 @@ export class AppComponent {
         questionNo: i,
         answered: false,
         mark: 0,
-        status: 'unanswered'
+        status: 'unanswered',
+        selectedOptions: 0
       });
     }
     console.log(this.arrayQuestion);
@@ -38,16 +51,20 @@ export class AppComponent {
     const questionIndex = this.arrayQuestion.findIndex(obj => obj.questionNo === question.questionNo);
     this.arrayQuestion[questionIndex].mark = 1;
     this.arrayQuestion[questionIndex].status = 'correct';
+    this.myStorage.setItem('questionans', JSON.stringify(this.arrayQuestion));
   }
   onWrong(question) {
     const questionIndex = this.arrayQuestion.findIndex(obj => obj.questionNo === question.questionNo);
     this.arrayQuestion[questionIndex].mark = 0;
     this.arrayQuestion[questionIndex].status = 'wrong';
+    this.myStorage.setItem('questionans', JSON.stringify(this.arrayQuestion));
   }
-  onAnswer(question) {
+  onAnswer(question, selectedOptions) {
     const questionIndex = this.arrayQuestion.findIndex(obj => obj.questionNo === question.questionNo);
     this.arrayQuestion[questionIndex].mark = 0;
     this.arrayQuestion[questionIndex].status = 'answered';
+    this.arrayQuestion[questionIndex].selectedOptions = selectedOptions;
+    this.myStorage.setItem('questionans', JSON.stringify(this.arrayQuestion));
   }
   openModal(template: TemplateRef<any>) {
     this.wrongAnswer = [];
@@ -63,6 +80,10 @@ export class AppComponent {
       }
     });
     this.modalRef = this.modalService.show(template);
+    this.myStorage.setItem('questionans', JSON.stringify(this.arrayQuestion));
+  }
+  reset() {
+    this.myStorage.clear();
   }
 
 }
